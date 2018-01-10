a399a6ebf5a46eaeb48276a24fecd33ce5fb221d
Seth Watkins <dev.seth.wat@gmail.com>
2018-01-10 11:40:07 -0600

    feat: Handle no data

--- app/src/main/java/wat/seth/dev/capstoneproject/fragments/QuakeListFragment.java
+++ app/src/main/java/wat/seth/dev/capstoneproject/fragments/QuakeListFragment.java
@@ -1,5 +1,6 @@
 package wat.seth.dev.capstoneproject.fragments;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
@@ -8,8 +9,10 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.Loader;
 import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceManager;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -21,8 +24,11 @@ import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.animation.LayoutAnimationController;
+import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import org.parceler.Parcels;
 
@@ -32,6 +38,7 @@ import wat.seth.dev.capstoneproject.MapActivity;
 import wat.seth.dev.capstoneproject.R;
 import wat.seth.dev.capstoneproject.adapters.QuakeListAdapter;
 import wat.seth.dev.capstoneproject.data.Earthquake;
+import wat.seth.dev.capstoneproject.handlers.QueryBuilder;
 import wat.seth.dev.capstoneproject.loaders.ApiFetchLoader;
 import wat.seth.dev.capstoneproject.utils.ErrorUtil;
 import wat.seth.dev.capstoneproject.utils.NetworkUtils;
@@ -45,6 +52,10 @@ import wat.seth.dev.capstoneproject.utils.NetworkUtils;
 public class QuakeListFragment extends Fragment implements LoaderManager.LoaderCallbacks<ArrayList<Earthquake>>,
         Preference.OnPreferenceChangeListener {
     private QuakeListAdapter mAdapter;
+    private FrameLayout errorView;
+    private Button errorViewButton;
+    private TextView errorViewText;
+    private ImageView errorImageView;
     private RecyclerView rv;
     FloatingActionButton fab;
     ProgressBar progressBar;
@@ -62,14 +73,22 @@ public class QuakeListFragment extends Fragment implements LoaderManager.LoaderC
 
     @Override
     public void onResume() {
+        if (!NetworkUtils.hasInternet(getContext())) {
+            progressBar.setVisibility(View.INVISIBLE);
+        }
+        /*
+        The loader is behaving incorrectly when returning from SettingsActivity and no internet
+        connection, unfortunately without a big rewrite loader needs to be reset here. TODO
+        */
+        getActivity().getSupportLoaderManager().restartLoader(loaderId, null, this);
         super.onResume();
 
     }
 
     @Override
     public void onPause() {
-        super.onPause();
         progressBar.setVisibility(View.VISIBLE);
+        super.onPause();
     }
 
     @Nullable
@@ -78,6 +97,10 @@ public class QuakeListFragment extends Fragment implements LoaderManager.LoaderC
         View view =  inflater.inflate(R.layout.list_fragment, container, false);
         //get view references
         rv = view.findViewById(R.id.my_rv);
+        errorView = view.findViewById(R.id.error_screen);
+        errorViewButton = view.findViewById(R.id.error_retry_button);
+        errorViewText = view.findViewById(R.id.error_display_message);
+        errorImageView = view.findViewById(R.id.error_image_view);
         rv.setLayoutManager(new LinearLayoutManager(getContext()));
         rv.setAdapter(mAdapter);
         fab = view.findViewById(R.id.world_map_fab);
@@ -97,20 +120,35 @@ public class QuakeListFragment extends Fragment implements LoaderManager.LoaderC
     @Override
     public void onLoadFinished(final Loader<ArrayList<Earthquake>> loader, final ArrayList<Earthquake> data) {
         //If the data is null determine source of data, handle appropriately, and do not update adapter.
-//        if (data == null) {
-//            if (loader.getId() == ApiFetchLoader.FETCH_FROM_API) {
-//                ErrorUtil.dataNullApi(rv, getContext(), getActivity().getSupportLoaderManager(),
-//                        loader.getId(), this);
-//            } else if (loader.getId() == ApiFetchLoader.FETCH_FROM_PROVIDER) {
-//                ErrorUtil.dataNullProvider(rv, getContext(), getActivity().getSupportLoaderManager(),
-//                        loader.getId(), this);
-//            }
-//            progressBar.setVisibility(View.INVISIBLE);
-//            return;
-//        }
-
-        mAdapter.setEarthquakes(data);
         progressBar.setVisibility(View.INVISIBLE);
+        if (data == null) {
+            if (loader.getId() == ApiFetchLoader.FETCH_FROM_API) {
+                errorView.setVisibility(View.VISIBLE);
+                errorViewButton.setVisibility(View.VISIBLE);
+                errorViewButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        getActivity().getSupportLoaderManager().restartLoader(ApiFetchLoader.FETCH_FROM_API,
+                                null, QuakeListFragment.this);
+                    }
+                });
+            } else if (loader.getId() == ApiFetchLoader.FETCH_FROM_PROVIDER) {
+                errorView.setVisibility(View.VISIBLE);
+                errorViewText.setText(getString(R.string.no_saved));
+                errorViewButton.setVisibility(View.INVISIBLE);
+                errorImageView.setImageDrawable(ContextCompat.getDrawable(getContext(), R.drawable.ic_bookmark_border_black_24dp));
+                errorViewButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        getActivity().getSupportLoaderManager().restartLoader(ApiFetchLoader.FETCH_FROM_API,
+                                null, QuakeListFragment.this);
+                    }
+                });
+            }
+            return;
+        }
+        errorView.setVisibility(View.INVISIBLE);
+        mAdapter.setEarthquakes(data);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -141,6 +179,7 @@ public class QuakeListFragment extends Fragment implements LoaderManager.LoaderC
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+
         return true;
     }
 }
